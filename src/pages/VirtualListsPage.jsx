0afd177d0a39e4727e0ff12e57499b99310a5b41
Victor <user@example.com>
2026-03-26 12:37:10 +0530

    -m VirtualListsPage

---
+++ src/pages/VirtualListsPage.jsx
@@ -0,0 +1,9 @@
+import VirtualList from "../virtual-lists/VirtualList";
+
+export default function VirtualListsPage() {
+    return (
+        <div className="page-container">
+            <VirtualList />
+        </div>
+    );
+}
