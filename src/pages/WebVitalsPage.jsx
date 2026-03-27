296119c6a42f2f682e1f7d343bfcea8f741bcc5d
Victor <user@example.com>
2026-03-27 11:56:47 +0530

    -m WebVitalsPage

---
+++ src/pages/WebVitalsPage.jsx
@@ -0,0 +1,9 @@
+import WebVitalsMonitor from "../web-vitals-monitoring/WebVitalsMonitor";
+
+export default function WebVitalsPage() {
+    return (
+        <div className="page-container">
+            <WebVitalsMonitor />
+        </div>
+    );
+}
